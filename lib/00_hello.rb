a6854dd15fa9797822b44bfde0aa51123efe1707
Alexandre Vanavermaete <alexandrevanavermaete@hotmail.fr>
2020-04-13 11:11:50 +0200

    testhello

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,7 +1,14 @@
 def hello
-
+    return "Hello!"
 end
+puts (hello)
 
 def greet(name)
+  
+    return "Hello, #{name}!" 
+    return "Hello, #{name}!" 
 
 end
+
+puts greet("Alice")
+puts greet ("Bob")
